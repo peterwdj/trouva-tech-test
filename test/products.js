ff528ab6d056d5587f462b0e3b7bf7a51c8d7690
Peter Johnstone <peter.wdj@gmail.com>
2018-02-07 16:24:52 +0000

    Undo change that broke tests

--- test/products.js
+++ test/products.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
 
-const { expect } = chai.expect;
+const expect = chai.expect;
 
 chai.use(chaiHttp);
 
