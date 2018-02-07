d29ee263c90060db03581ca57cd3e71de74d4b7b
Peter Johnstone <peter.wdj@gmail.com>
2018-02-07 16:06:22 +0000

    Modify formatting to conform to eslint Airbnb style guide

--- test/products.js
+++ test/products.js
@@ -1,10 +1,8 @@
-'use strict';
-
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
 
-const expect = chai.expect;
+const { expect } = chai.expect;
 
 chai.use(chaiHttp);
 
@@ -12,10 +10,10 @@ describe('Products', () => {
   describe('/GET products', () => {
     it('should return status code 200', (done) => {
       chai.request(server)
-      .get('/products')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-      });
+        .get('/products')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+        });
       done();
     });
   });
