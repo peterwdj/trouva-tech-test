4ee5c0b01201a8821259a1197e417f4d78432d26
Peter Johnstone <peter.wdj@gmail.com>
2018-02-08 22:13:05 +0000

    Send hello world in GET route

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
-app.get('/products', () => {});
+app.get('/products', (req, res) => {
+  res.send('Hello, world');
+})
 
 app.listen(port);
 
