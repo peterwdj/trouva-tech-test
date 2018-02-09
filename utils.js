2ddfa9ddca3aabf31bcdbbfc07a3946e7bcd6b63
Peter Johnstone <peter.wdj@gmail.com>
2018-02-09 15:34:04 +0000

    Tidy up formatting per eslint

--- utils.js
+++ utils.js
@@ -1,14 +1,12 @@
-'use strict';
-
 const Product = require('./lib/product');
 
 function createObjects(array) {
-  let products = [];
-  array.forEach(function (item) {
-    let product = new Product(item.name, item._id, item.thumbnail.url, item.description);
+  const products = [];
+  array.forEach((item) => {
+    const product = new Product(item.name, item._id, item.thumbnail.url, item.description);
     products.push(product);
   });
   return products;
-};
+}
 
 module.exports.createObjects = createObjects;
