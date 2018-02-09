1b70f13eded9a052d9dbee58d26a3660b1efcfbf
Peter Johnstone <peter.wdj@gmail.com>
2018-02-09 11:33:06 +0000

    Rename product-test.js to server-test.js

---
+++ test/server-test.js
@@ -0,0 +1,34 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('/GET products', () => {
+  it('should return status code 200', (done) => {
+    chai.request(server)
+      .get('/products')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+  it('should return an array', (done) => {
+    chai.request(server)
+      .get('/products')
+      .end((err, res) => {
+        expect(res.body).to.be.a('array');
+        done();
+      });
+  });
+  it('should contain 500 products', (done) => {
+    chai.request(server)
+      .get('/products')
+      .end((err, res) => {
+        expect(res.body.length).to.equal(500);
+        done();
+      });
+  });
+});
