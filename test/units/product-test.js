08c4d60a360438088285302651d658cf2c04acea
Peter Johnstone <peter.wdj@gmail.com>
2018-02-09 12:42:00 +0000

    Test creating Product object from a JSON with same structure as products.json

--- test/units/product-test.js
+++ test/units/product-test.js
@@ -1,12 +1,20 @@
 'use strict';
 
 const chai = require('chai');
-const Product = require('./../../lib/product')
+const Product = require('./../../lib/product');
 
 const expect = chai.expect;
 
 describe('Product', () => {
-  let bowTie = new Product();
+  const bowTie = new Product();
+  const json = {
+    '_id':'1234',
+    'name':'Bow tie',
+    'images':[
+      {'desktop':'url.jpg'}
+    ],
+    'description':'Bow ties are cool'
+  };
   it('has a name', () => {
     expect(bowTie).to.have.property('name');
   });
@@ -18,5 +26,12 @@ describe('Product', () => {
   });
   it('has a description', () => {
     expect(bowTie).to.have.property('description');
+  });
+  it('creates a product with values from a json object', () => {
+    let newBowTie = new Product(json.name, json._id, json.images[0].desktop, json.description);
+    expect(newBowTie.name).to.equal('Bow tie');
+    expect(newBowTie.id).to.equal('1234');
+    expect(newBowTie.image).to.equal('url.jpg');
+    expect(newBowTie.description).to.equal('Bow ties are cool');
   });
 });
