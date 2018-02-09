9ea21bf18fda175a1e78c901ce2eae2591206fb6
Peter Johnstone <peter.wdj@gmail.com>
2018-02-09 09:36:35 +0000

    Send array of products at GET /products route

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const fs = require('fs');
 
 const port = 8000;
 const app = express();
 
 app.get('/products', (req, res) => {
-  res.send('Hello, world');
+  var json = JSON.parse(fs.readFileSync('products.json'));
+  res.send(json.products);
 })
 
 app.listen(port);
