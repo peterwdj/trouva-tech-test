7fb5de572a5d7d378aaca13c4a3a91c3df3f6ba1
Peter Johnstone <peter.wdj@gmail.com>
2018-02-09 12:07:03 +0000

    DRY out tests

--- test/units/product-test.js
+++ test/units/product-test.js
@@ -6,20 +6,17 @@ const Product = require('./../../lib/product')
 const expect = chai.expect;
 
 describe('Product', () => {
+  let bowTie = new Product();
   it('has a name', () => {
-    let bowTie = new Product();
     expect(bowTie).to.have.property('name');
   });
   it('has an ID', () => {
-    let bowTie = new Product();
     expect(bowTie).to.have.property('id');
   });
   it('has an image', () => {
-    let bowTie = new Product();
     expect(bowTie).to.have.property('image');
   });
   it('has a description', () => {
-    let bowTie = new Product();
     expect(bowTie).to.have.property('description');
   });
 });
