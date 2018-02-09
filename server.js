1cd2822a581cee5298b65972b86dfad74477d32c
Peter Johnstone <peter.wdj@gmail.com>
2018-02-09 15:14:51 +0000

    Create product objects array

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+'use strict';
+
 const express = require('express');
 const fs = require('fs');
+const Product = require('./lib/product')
 
+const products = JSON.parse(fs.readFileSync('products.json')).products;
 const port = 8000;
 const app = express();
 
 app.get('/products', (req, res) => {
-  const json = JSON.parse(fs.readFileSync('products.json'));
-  res.send(json.products);
+  let productObjects = [];
+  products.forEach(function(product) {
+    let productObject = new Product(product.name, product._id, product.thumbnail.url, product.description);
+    productObjects.push(productObject);
+  });
+  res.send(productObjects);
 });
 
 app.listen(port);
