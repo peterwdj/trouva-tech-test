107e00ab9bd281a98a17036a846104c8b1daacfa
Peter Johnstone <peter.wdj@gmail.com>
2018-02-09 11:41:41 +0000

    Add tests for existence of ID, image, description properties

--- test/units/product-test.js
+++ test/units/product-test.js
@@ -7,7 +7,19 @@ const expect = chai.expect;
 
 describe('Product', () => {
   it('has a name', () => {
-    let bowTie = new Product('Bow tie');
+    let bowTie = new Product();
     expect(bowTie).to.have.property('name');
+  });
+  it('has an ID', () => {
+    let bowTie = new Product();
+    expect(bowTie).to.have.property('id');
+  });
+  it('has an image', () => {
+    let bowTie = new Product();
+    expect(bowTie).to.have.property('image');
+  });
+  it('has a description', () => {
+    let bowTie = new Product();
+    expect(bowTie).to.have.property('description');
   });
 });
