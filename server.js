7c6093fcea7fd3b6f9d01ee19d647cf8e1eee3f7
Peter Johnstone <peter.wdj@gmail.com>
2018-02-10 14:07:16 +0000

    Use EJS rendering engine, render /products ejs page

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const json = JSON.parse(fs.readFileSync('products.json'));
 const port = 8000;
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/products', (req, res) => {
-  const products = utils.createObjects(json.products);
-  res.send(products);
+  let products = utils.createObjects(json.products);
+  res.render('pages/products', { products });
 });
 
 app.listen(port);
