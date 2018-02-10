e0b177f98d83a792dad76a74132d77bab66f6026
Peter Johnstone <peter.wdj@gmail.com>
2018-02-10 14:25:48 +0000

    DRY out utils tests

--- test/utils-test.js
+++ test/utils-test.js
@@ -18,14 +18,14 @@ describe('Utils', () => {
       }
     ];
 
+    const products = utils.createObjects(productArray);
+
     it('should return an array', (done) => {
-      let products = utils.createObjects(productArray);
       expect(products).to.be.a('array');
       done();
     });
 
     it('should contain 500 products', (done) => {
-      let products = utils.createObjects(productArray);
       expect(products.length).to.equal(1);
       done();
     });
