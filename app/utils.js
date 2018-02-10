849dd52120193e30b59ada5c730c7d93a72c9665
Peter Johnstone <peter.wdj@gmail.com>
2018-02-10 23:21:04 +0000

    Fix bug

--- app/utils.js
+++ app/utils.js
@@ -5,7 +5,7 @@ const Product = require('./lib/product');
 function createObjects(array) {
   const products = [];
   array.forEach((item) => {
-    const product = new Product(item.name, item._id, item.thumbnail.url, item.description, product.price);
+    const product = new Product(item.name, item._id, item.thumbnail.url, item.description, item.price);
     products.push(product);
   });
   return products;
