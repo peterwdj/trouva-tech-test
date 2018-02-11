41824fbe56cacf7b07a07cff2c5d1b4b061a04a2
Peter Johnstone <peter.wdj@gmail.com>
2018-02-11 18:24:50 +0000

    Add test for addProduct function

--- test/units/collection-test.js
+++ test/units/collection-test.js
@@ -1,19 +1,35 @@
 'use strict';
 
 const chai = require('chai');
-const Collection = require('./../../app/lib/collection.js');
+const Collection = require('./../../app/lib/collection');
+const Product = require('./../../app/lib/product');
 
 const expect = chai.expect;
 
 describe('Collection', () => {
-  const homeStuff = new Collection();
+  const collection = new Collection('Home Stuff');
+  const fakeVaseObject = {
+    _id: '42',
+    name: 'Scandinavian vase',
+    images: [
+      { desktop: 'url.jpg' },
+    ],
+    description: 'Beautiful little vase, ideal for tables or shelves.',
+    price: '4900',
+  };
   it('has a name', () => {
-    expect(homeStuff).to.have.property('name');
+    expect(collection).to.have.property('name');
   });
   it('has a products property', () => {
-    expect(homeStuff).to.have.property('products');
+    expect(collection).to.have.property('products');
   });
   it('has an empty array assigned to its products property by default', () => {
-    expect(homeStuff.products).to.be.a('array');
+    expect(collection.products).to.be.a('array');
+  });
+  describe('addProduct()', () => {
+    it('adds a new product to this.products', () => {
+      collection.addProduct(fakeVaseObject);
+      expect(collection.products.length).to.equal(1);
+    });
   });
 });
