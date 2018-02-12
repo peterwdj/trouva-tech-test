ec93c2f79df8446dd688fa854078d37bdf3b364a
Peter Johnstone <peter.wdj@gmail.com>
2018-02-12 10:55:34 +0000

    Injects database into routes, seeds products collection

--- app/server.js
+++ app/server.js
@@ -2,8 +2,11 @@
 
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
+const utils = require('./utils');
 const routes = require('./routes/routes')
 
+const json = JSON.parse(fs.readFileSync('products.json'));
 const url = 'mongodb://localhost:27017/trouva_dev'
 const port = 8000;
 const app = express();
@@ -13,8 +16,12 @@ app.set('views', './app/views');
 
 MongoClient.connect(url, (err, db) => {
   const database = db.db('trouva_dev');
+  const products = utils.createObjects(json.products);
+  for (let product of products) {
+    database.collection('products').save(product);
+  };
   if (err) return console.log(err);
-  routes(app);
+  routes(app, database);
 });
 
 app.listen(port);
