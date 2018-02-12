a7e98bdd33a536ce08e5af293febaeb780afb976
Peter Johnstone <peter.wdj@gmail.com>
2018-02-12 10:21:51 +0000

    Reflects updated _id property (instead of id)

--- test/units/product-test.js
+++ test/units/product-test.js
@@ -20,7 +20,7 @@ describe('Product', () => {
     expect(bowTie).to.have.property('name');
   });
   it('has an ID', () => {
-    expect(bowTie).to.have.property('id');
+    expect(bowTie).to.have.property('_id');
   });
   it('has an image', () => {
     expect(bowTie).to.have.property('image');
@@ -34,7 +34,7 @@ describe('Product', () => {
   it('creates a product with values from a json object', () => {
     const newBowTie = new Product(json.name, json._id, json.images[0].desktop, json.description, json.price);
     expect(newBowTie.name).to.equal('Bow tie');
-    expect(newBowTie.id).to.equal('1234');
+    expect(newBowTie._id).to.equal('1234');
     expect(newBowTie.image).to.equal('url.jpg');
     expect(newBowTie.description).to.equal('Bow ties are cool');
     expect(newBowTie.price).to.equal('3200');
