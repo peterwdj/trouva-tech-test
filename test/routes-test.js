5e940b0353ae89e05943b0e4e3f747495bc26828
Peter Johnstone <peter.wdj@gmail.com>
2018-02-12 23:39:13 +0000

    Cleans test db after each test

--- test/routes-test.js
+++ test/routes-test.js
@@ -4,46 +4,60 @@ process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const MongoClient = require('mongodb').MongoClient;
+const config = require('./../app/config').getEnv(process.env.NODE_ENV);
 const server = require('../app/server');
 
 const expect = chai.expect;
+const url = `mongodb://localhost:27017/${config.database}`;
 
 chai.use(chaiHttp);
 
-describe('/GET products', () => {
-  it('should return status code 200', (done) => {
-    chai.request(server)
+describe('Test API routes', () => {
+
+  afterEach(function(done) {
+    MongoClient.connect(url, (err, db) => {
+      const database = db.db(config.database);
+      database.collection('collections').drop();
+      done();
+    });
+  });
+
+  describe('/GET products', () => {
+    it('should return status code 200', (done) => {
+      chai.request(server)
       .get('/products')
       .end((err, res) => {
         expect(res).to.have.status(200);
         done();
       });
+    });
   });
-});
 
-describe('/GET collections', () => {
-  it('should return status code 200', (done) => {
-    chai.request(server)
+  describe('/GET collections', () => {
+    it('should return status code 200', (done) => {
+      chai.request(server)
       .get('/collections')
       .end((err, res) => {
         expect(res).to.have.status(200);
         done();
       });
+    });
   });
-});
 
-describe('/POST collections', () => {
-  const collection = {
-    name: 'Homewares',
-    products: [],
-  };
-  it('should POST a new collection', (done) => {
-    chai.request(server)
+  describe('/POST collections', () => {
+    const collection = {
+      name: 'Homewares',
+      products: [],
+    };
+    it('should POST a new collection', (done) => {
+      chai.request(server)
       .post('/collections')
       .send(collection)
       .end((err, res) => {
         expect(res).to.have.status(200);
         done();
       });
+    });
   });
 });
