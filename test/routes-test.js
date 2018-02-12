a6f0f530c5c332086bc71baa3836ab44bc8dbfd9
Peter Johnstone <peter.wdj@gmail.com>
2018-02-12 15:22:23 +0000

    Tests res status for /POST collections

--- test/routes-test.js
+++ test/routes-test.js
@@ -8,7 +8,7 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('GET /products', () => {
+describe('/GET products', () => {
   it('should return status code 200', (done) => {
     chai.request(server)
       .get('/products')
@@ -19,12 +19,28 @@ describe('GET /products', () => {
   });
 });
 
-describe('GET /collections', () => {
+describe('/GET collections', () => {
   it('should return status code 200', (done) => {
     chai.request(server)
       .get('/collections')
       .end((err, res) => {
         expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
+
+describe('/POST collections', () => {
+  const collection = {
+    name: 'Homewares',
+  };
+  it('should POST a new collection', (done) => {
+    chai.request(server)
+      .post('/collections')
+      .send(collection)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        console.log('Hello, world');
         done();
       });
   });
