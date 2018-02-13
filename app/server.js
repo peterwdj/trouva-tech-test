25facb5afbd3a9bb0b2896187026199978166592
Peter Johnstone <peter.wdj@gmail.com>
2018-02-13 11:31:50 +0000

    Searches for static files in app/public

--- app/server.js
+++ app/server.js
@@ -17,6 +17,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+app.use(express.static('./app/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
