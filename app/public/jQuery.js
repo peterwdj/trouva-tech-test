ae6f0646b1e6513f5a5a3bdfc48c739b834e78ad
Peter Johnstone <peter.wdj@gmail.com>
2018-02-14 09:41:33 +0000

    Fixes linting issues

--- app/public/jQuery.js
+++ app/public/jQuery.js
@@ -1,10 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $('.add-to-collection').click(function() {
-    let productId = ($(this).parent().prop('id'));
-    let collectionId = $(`#${productId} select :selected`).val();
-    $.post(`/collections/${collectionId}`, {
-      productId: productId,
-      collectionId: collectionId
-    });
+    const productId = ($(this).parent().prop('id'));
+    const collectionId = $(`#${productId} select :selected`).val();
+    $.post(`/collections/${collectionId}`, { productId, collectionId });
   });
 });
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -6,9 +6,9 @@ const ObjectID = require('mongodb').ObjectID;
 function routes(app, db) {
   app.get('/products', (req, res) => {
     db.collection('products').find().toArray((err, products) => {
-      db.collection('collections').find().toArray((err, collections) => {
-        res.render('pages/products', { products: products, collections: collections });
-      })
+      db.collection('collections').find().toArray((error, collections) => {
+        res.render('pages/products', { products, collections });
+      });
     });
   });
 
@@ -27,7 +27,7 @@ function routes(app, db) {
   app.post('/collections/:id', (req, res) => {
     const collectionId = req.body.collectionId.toString();
     const productId = req.body.productId;
-    db.collection('products').find( { _id: productId }).toArray((err, result) => {
+    db.collection('products').find({ _id: productId }).toArray((err, result) => {
       db.collection('collections').update(
         { _id: ObjectID(collectionId) },
         { $push: { products: result[0] } }
