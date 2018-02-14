99ede8d1da60d49cec194c7de1ffda1a1f6e3490
Peter Johnstone <peter.wdj@gmail.com>
2018-02-14 10:06:32 +0000

    Flashes a message informing the user that their item has been adedd to their collection

--- app/public/jQuery.js
+++ app/public/jQuery.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
   $('.add-to-collection').click(function() {
-    const productId = ($(this).parent().prop('id'));
+    const productId = $(this).parent().prop('id');
     const collectionId = $(`#${productId} select :selected`).val();
     $.post(`/collections/${collectionId}`, { productId, collectionId });
+    $(this).parent().append('<p class="confirmation-message">Congratulations! This item has been added to your collection.</p>');
   });
 });
